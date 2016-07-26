5311618644c7564cc2f7299f7872c9e54e618e41
djado <jdhabolt@djaango.com>
2016-07-26 10:54:29 -0700

    First bit of functionality

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// show 3 imgs at a time
+// rank imgs
+// store, calc, display resulting data
+
+// randomly show 3 imgs at app start, side-by-side
+// instructional text to click on product most likely to buy
+// use eventlistener to track clicks for each img
+// log to console the selected product, and its total votes so far
+// display next 3 imgs
+// show 'See Totals' button after every 15 user votes that shows listing of all objects and their votes
+// --- sort the list by vote number
+// ------ calc % of votes per appearance for each, show in separate list
+// --------- another analysis metric based on num of clicks, present all data points in table instead of list
+
+// Choose a custom web font (Links to an external site)
+// Choose a custom color palette (Links to an external site)
