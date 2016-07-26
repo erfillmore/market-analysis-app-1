a5f5a5b0e0d26690bafdbc47ba4fcb2a3cd05c93
djado <jdhabolt@djaango.com>
2016-07-26 15:51:10 -0700

    Basic functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-// show 3 imgs at a time
-// rank imgs
-// store, calc, display resulting data
+// XXX show 3 imgs at a time
+// XXX rank imgs
+// XXX store, calc, display resulting data
 
 // randomly show 3 imgs at app start, side-by-side
 // instructional text to click on product most likely to buy
-// use eventlistener to track clicks for each img
-// log to console the selected product, and its total votes so far
-// display next 3 imgs
-// show 'See Totals' button after every 15 user votes that shows listing of all objects and their votes
-// --- sort the list by vote number
+// XXX use eventlistener to track clicks for each img
+// XXX log to console the selected product, and its total votes so far
+// XXX display next 3 imgs
+// XXX show 'See Totals' button after every 15 user votes that shows listing of all objects and their votes
+// XXX --- sort the list by vote number
 // ------ calc % of votes per appearance for each, show in separate list
 // --------- another analysis metric based on num of clicks, present all data points in table instead of list
 
